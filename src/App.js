0082af0e8b88ab30dda8f883cf42cb28ef9821da
Nicolas Coronado <nicolasdgioc15@gmail.com>
2025-07-29 15:23:39 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mi Primera App de React</h1>
+        <p>¡Bienvenido a tu nuevo proyecto!</p>
+        <button onClick={() => alert('¡Hola desde React!')}>
+          Hacer clic aquí
+        </button>
       </header>
     </div>
   );
